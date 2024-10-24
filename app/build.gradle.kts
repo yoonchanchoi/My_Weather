0eef22a4a4d366feadff820f6a109261d864ae27
Choi YoonChan <choyc9239@gmail.com>
2024-10-24 23:14:07 +0900

    [Feat/#3] 멀티모듈 및 Hilt 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,10 @@ android {
 
 dependencies {
 
+    implementation(project(":presentation"))
+    implementation(project(":domain"))
+    implementation(project(":data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
